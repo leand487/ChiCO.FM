402c2d98576beb0eca8f35828f98f8fbcff3792e
Eric <Eric.Leander@gmail.com>
2012-06-06 15:20:35 -0500

    Room Changed to ChiCO

--- examples/bopcount.js
+++ examples/bopcount.js
@@ -7,7 +7,7 @@
 var Bot    = require('../index');
 var AUTH   = 'auth+live+2cdbb9bd018b4d3af371b29471ce903b9413083a';
 var USERID = '4fc90519eb35c1533d000006';
-var ROOMID = '4fc90771eb35c1533d00000d';
+var ROOMID = '4e3c380114169c4fda19da3b';
 
 var bot = new Bot(AUTH, USERID, ROOMID);
 
